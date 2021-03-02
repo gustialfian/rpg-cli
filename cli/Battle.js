bb07d9023c5534c8494c2e412f8092b6a69d994b
Gusti Alfian M. P <gustialfianmp@gmail.com>
2021-03-02 18:10:02 +0700

    [UPDATE]: next turn

--- cli/Battle.js
+++ cli/Battle.js
@@ -3,40 +3,57 @@ const { useState } = require('react');
 const { Text, Box, Newline } = require('ink');
 
 const { initBattle } = require('../game/sample-state')
+const { calcBattle, getPlayerIndexByName } = require('../game/battle')
 
 const importJsx = require('import-jsx')
 const TextInput = importJsx('ink-text-input').default
 
-/**
-  {
-    action: 'attack',
-    actor: getPlayerIndexByName(initBattle, 'Jowy'),
-    target: getPlayerIndexByName(initBattle, 'slime'),
-  }
- */
+const baseCommand = {
+  action: '',
+  actor: -1,
+  target: -1,
+}
 
 function Battle(params) {
+  const [battle, setBattle] = useState(initBattle)
+
   const [mode, setMode] = useState('action')
   const [input, setInput] = useState('')
-  // const [pick, setPick] = useState(-1)
-  const [command, setCommand] = useState({
-    action: '',
-    actor: -1,
-    target: -1,
-  })
-  console.log(`hai`, command)
+  const [command, setCommand] = useState(baseCommand)
+
+
+  const slimeCommand = {
+    action: 'attack',
+    actor: getPlayerIndexByName(initBattle, 'slime'),
+    target: getPlayerIndexByName(initBattle, 'Jowy'),
+  }
+  const dummyCommand = {
+    action: 'attack',
+    actor: getPlayerIndexByName(initBattle, 'dummy'),
+    target: getPlayerIndexByName(initBattle, 'dummy'),
+  }
+  console.log(`hai`, [command, slimeCommand, dummyCommand])
 
   const handleSubmit = (val) => {
     if (mode == 'action' && val == 'a') {
       setMode('attack')
-      setCommand({...command, action: 'attack'})
-      setInput('')
+      setCommand({ ...command, action: 'attack' })
+    }
+    if (mode == 'action' && val == 'w') {
+      setMode('wait')
+      setCommand({ ...command, action: 'wait' })
     }
     if (mode == 'attack') {
       setMode('action')
-      setCommand({...command, actor: 0, target: val})
-      setInput('')
+      setCommand({ ...command, actor: 0, target: val })
+      setBattle(calcBattle(battle, [command, slimeCommand, dummyCommand]))
     }
+    if (mode == 'wait') {
+      setMode('action')
+      setCommand({ ...command, actor: 0, target: 0 })
+      setBattle(calcBattle(battle, [command, slimeCommand, dummyCommand]))
+    }
+    setInput('')
   }
 
   return (
@@ -46,13 +63,13 @@ function Battle(params) {
       </Box>
       <Box minHeight={10}>
         <Box borderStyle={"round"} width={50}>
-          <Profile player={initBattle.players[0]} />
+          <Profile player={battle.players[0]} />
         </Box>
 
         <Box borderStyle={"round"} width={30}>
           <Text>
             <Text>Players: </Text> <Newline />
-            <ListPlayer players={initBattle.players} />
+            <ListPlayer players={battle.players} />
           </Text>
         </Box>
       </Box>
@@ -70,19 +87,21 @@ function Battle(params) {
 }
 
 function Profile({ player }) {
-  return <Text>
-    <Text>{player.name}</Text> <Newline />
-    <Text>HP: {player.hp}</Text> <Newline />
-    <Newline />
-    <Text>Equipment</Text> <Newline />
-    <Text>Right: {player.equipment.right.name}</Text> <Newline />
-    <Text>Left:  {player.equipment.left.name}</Text> <Newline />
-    <Text>Armor: {player.equipment.armor.name}</Text> <Newline />
-    <Newline />
-    <Text>Power</Text> <Newline />
-    <Text>Melle:  {player.power.melle}</Text> <Newline />
-    <Text>Armor: {player.power.armor}</Text> <Newline />
-  </Text>
+  return (
+    <Text>
+      <Text>{player.name}</Text> <Newline />
+      <Text>HP: {player.hp}</Text> <Newline />
+      <Newline />
+      <Text>Equipment</Text> <Newline />
+      <Text>Right: {player.equipment.right.name}</Text> <Newline />
+      <Text>Left:  {player.equipment.left.name}</Text> <Newline />
+      <Text>Armor: {player.equipment.armor.name}</Text> <Newline />
+      <Newline />
+      <Text>Power</Text> <Newline />
+      <Text>Melle:  {player.power.melle}</Text> <Newline />
+      <Text>Armor: {player.power.armor}</Text> <Newline />
+    </Text>
+  )
 }
 
 function ListPlayer({ players }) {
@@ -100,7 +119,11 @@ function Menu({ mode }) {
   }
 
   if (mode == 'attack') {
-    return <Text>pick target (index)</Text>
+    return <Text>Attacking pick target (index)</Text>
+  }
+
+  if (mode == 'wait') {
+    return <Text>waiting...</Text>
   }
 
   return <Text>Unknown</Text>
@@ -113,6 +136,10 @@ function Logs({ logs }) {
       <Newline />
     </Text>
   ))
+}
+
+function sendCommand(commands) {
+  console.log(`sendCommand...`)
 }
 
 module.exports = Battle
